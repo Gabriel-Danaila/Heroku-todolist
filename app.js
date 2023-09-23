61e0d09640f568119dc00e7fc78c79ddaf12503a
Gabriel Danaila <gabriel.2021@yahoo.com>
2023-09-23 14:40:43 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,139 @@
+const express = require("express");
+const app = express();
+/* cand dowloadezi de pe net vine cu pachetul jason dar fara node-modules asa ca trebuie sa faci in acel folder npm i (adica npm install si se uita la dependecy care sunt in json file si le instaleaza in acel folder) */
+/* ca sa nu mai dispara memoria cand facem restart atunci vom folosii data base mongoDB and mongoose deci putem sa stergem const items si const workItems */
+// const items = ["Buy Food", "Cook Food", "Eat Food"];
+// const workItems = [];
+const _ = require("lodash");
+const mongoose = require("mongoose");
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect(
+  "mongodb+srv://admin-gabriel:Test12345@cluster0.ovucifz.mongodb.net/todolistDB"
+);
+const itemsSchema = {
+  name: String,
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!",
+});
+const item2 = new Item({
+  name: "Hit this + button to add a new item",
+});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item",
+});
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema],
+};
+
+const List = mongoose.model("List", listSchema);
+
+app.get("/", function (req, res) {
+  Item.find({}, function (err, foundItems) {
+    // console.log(foundItems); aicea iti dadea an empty array asa ca te poti folosii de lungimea ei ca sa verifici
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved default itmes to DB.");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+    }
+  });
+});
+
+app.get("/:customListName", function (req, res) {
+  const customListName = _.capitalize(req.params.customListName);
+  List.findOne({ name: customListName }, function (err, foundList) {
+    if (!err) {
+      if (!foundList) {
+        // Create a new list
+        const list = new List({
+          name: customListName,
+          items: defaultItems,
+        });
+        list.save();
+        res.redirect("/" + customListName);
+      } else {
+        // Show an  existing list
+        res.render("list", {
+          listTitle: foundList.name,
+          newListItems: foundList.items,
+        });
+      }
+    }
+  });
+});
+
+app.post("/", function (req, res) {
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+  const item = new Item({
+    name: itemName,
+  });
+
+  if (listName === "Today") {
+    item.save();
+    res.redirect("/");
+  } else {
+    List.findOne({ name: listName }, function (err, foundList) {
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
+});
+
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === "Today") {
+    Item.findByIdAndRemove(checkedItemId, function (err) {
+      if (!err) {
+        console.log("Successfuly deleted checked item.");
+        res.redirect("/");
+      }
+    });
+  } else {
+    List.findOneAndUpdate(
+      { name: listName },
+      { $pull: { items: { _id: checkedItemId } } },
+      function (err, foundList) {
+        if (!err) {
+          res.redirect("/" + listName);
+        }
+      }
+    );
+  }
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log("Server has started successfully");
+});
+/* daca run on locallhost:3000 poti addlocallhost:3000/cars , locallhost:3000/Jobs etc  */
+/* daca run from heroku atunci ai numai https://sleepy-lake-64693.herokuapp.com  cu Today list and items*/
